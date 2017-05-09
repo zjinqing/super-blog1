dd3a9e5fbde684e59c1271935b3d26d88bd81113
zy <932725002@qq.com>
2017-05-09 14:20:29 +0800

    初始化项目

---
+++ routes.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+const express = require('express');
+const router = express.Router();
+//引入首页的路由文件
+const home = require('./routes/home');
+//首页
+// home.index一个函数
+router.get('/',home.index);
+
+
+
+
+
+module.exports = router;
